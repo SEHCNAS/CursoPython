f8a2b34a5db3c0f0f258222e497be23603fe5868
Gabriel <thebug@outlook.com.br>
2021-11-03 21:27:34 -0300

    funções append, remove e reverse de um tipo lista

---
+++ Lista.py
@@ -0,0 +1,21 @@
+Lista = []
+# Tamanho da lista
+print(len(Lista))
+
+# Adicionar itens na lista
+Lista.append(0)
+Lista.append(5)
+print(Lista)
+print(len(Lista))
+
+Lista_2 = ['texto', 1, 'texto_2', 2]
+print(Lista_2)
+
+
+# Remover item da lista
+Lista_2.remove(1)
+print(Lista_2)
+
+# Reverter uma lista
+Lista_2.reverse()
+print(Lista_2)
