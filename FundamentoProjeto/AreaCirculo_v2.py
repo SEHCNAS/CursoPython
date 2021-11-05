d9679acb1c3de59b52d9bf3cf8b633c5d975ab40
Gabriel <thebug@outlook.com.br>
2021-11-04 21:10:31 -0300

    v2: import do math para usar o pi

---
+++ FundamentoProjeto/AreaCirculo_v2.py
@@ -0,0 +1,11 @@
+# pegar pi importando toda função math
+# import math
+# RaioCirculo = 15.3
+# AreaCirculo = math.pi * (RaioCirculo ** 2)
+# print('A area do circulo é %.2f' % (AreaCirculo))
+
+# importar somente a função pi
+from math import pi
+RaioCirculo = 15.3
+AreaCirculo = pi * (RaioCirculo ** 2)
+print('A area do circulo é %.2f' % (AreaCirculo))
