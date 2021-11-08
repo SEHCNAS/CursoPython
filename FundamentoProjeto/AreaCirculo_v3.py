9ab3ba45e1a12c5ec868a8b228e9853c398f9547
Gabriel <thebug@outlook.com.br>
2021-11-08 20:17:15 -0300

    Adição de um laço condicional

--- FundamentoProjeto/AreaCirculo_v3.py
+++ FundamentoProjeto/AreaCirculo_v3.py
@@ -1,6 +1,7 @@
-#! python
+#!python
 # pegar pi importando toda função math
 import math
-RaioCirculo = input('Informe o raio: ')
-AreaCirculo = math.pi * (float(RaioCirculo) ** 2)
-print('A area do circulo é %.2f' % (AreaCirculo))
+if __name__ == '__main__':
+    RaioCirculo = input('Informe o raio: ')
+    AreaCirculo = math.pi * (float(RaioCirculo) ** 2)
+    print('A area do circulo é %.2f' % (AreaCirculo))
