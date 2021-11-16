a45276ac47182a8e092eb023014f16aa7b643974
Gabriel <thebug@outlook.com.br>
2021-11-16 16:33:05 -0300

    Exemplo de como usar o break_continue no laço for

---
+++ EstruturasControle/Break_Continue.py
@@ -0,0 +1,11 @@
+# break - sai do laço de repetição
+for y in range(1, 11):
+    if y == 5:
+        break
+    print(y)
+
+# Continue - avança para o proximo valor
+for x in range(1, 11):
+    if x % 2 == 0:
+        continue
+    print(x)
