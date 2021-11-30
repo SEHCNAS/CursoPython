ed71bd2a1dec5da0ed5656cebf6e7eda60724848
Gabriel Sanches <thebug@outlook.com.br>
2021-11-30 20:54:10 -0300

    Exemplo de try Finally

---
+++ ManipulacaoArquivos/LendoArquivo_V4.py
@@ -0,0 +1,12 @@
+# Pega o arquivo
+arquivo = open('ManipulacaoArquivos\Pessoas.csv')
+try:
+    for registro in arquivo:
+        # Strip retira o conteudo da string
+        print('Nome: {}, Idade: {}'.format(registro.strip().split(',')))
+        #print('Nome: {}, Idade: {}'.format(*registro.strip().split(',')))
+
+finally:
+    # Fecha o arquivo
+    print('evento finally')
+    arquivo.close()
