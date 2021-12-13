57bb95bfeebc70814808807c311218e1f2b14291
Gabriel <thebug@outlook.com.br>
2021-12-13 14:38:08 -0300

    Desafio de leitura de arquivo csv

--- ManipulacaoArquivos/DesafioIbge.py
+++ ManipulacaoArquivos/DesafioIbge.py
@@ -1,1 +1,10 @@
 # Ler 4 e 9 campo, pulando a primeira linha
+import csv
+with open('ManipulacaoArquivos\desafio-ibge.csv', newline='') as entrada:
+    count = 0
+    reader = csv.DictReader(entrada)
+    for row in reader:
+        print("{} : {}".format(row['nome_desti'], row['nome_orige']))
+        count = count + 1
+
+print('O numero de registros é {}'.format(count))
