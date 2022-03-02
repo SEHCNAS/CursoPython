c95f0ab2db6dcdf552dad764235c77fc38b6fe7d
Gabriel Sanches <thebug@outlook.com.br>
2022-03-02 20:55:24 -0300

    Exemplo de como fazer um select em python

---
+++ bancoDados/SelecionaDados.py
@@ -0,0 +1,18 @@
+from mysql.connector.errors import ProgrammingError
+from db import nova_conexao
+
+sql = """
+    SELECT * FROM contatos
+"""
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+        # não usa para buscar muitos dados, pode afetar o desempenho
+        contatos = cursor.fetchall()
+    except ProgrammingError as e:
+        print(f'Erro {e.msg}')
+    else:
+        for contato in contatos:
+            print(f'{contato[2]:2d} - {contato[0]:20s} telefone: {contato[1]}')
