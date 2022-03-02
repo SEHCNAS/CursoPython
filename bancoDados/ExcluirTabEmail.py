752224c0684f9c82f3544f0b2e900a1a807dac0b
Gabriel Sanches <thebug@outlook.com.br>
2022-03-02 19:59:49 -0300

    Bloco para executar sentenças sql que exclui a tabela emails

---
+++ bancoDados/ExcluirTabEmail.py
@@ -0,0 +1,16 @@
+from db import nova_conexao
+from mysql.connector import ProgrammingError
+
+ExluirTabEmail = """
+    Drop table emails
+"""
+
+try:
+    with nova_conexao() as conexao:
+        try:
+            cursor = conexao.cursor()
+            cursor.execute(ExluirTabEmail)
+        except ProgrammingError as e:
+            print(f"Erro conexao: {e.msg}")
+except ProgrammingError as e:
+    print(f"Erro conexao: {e.msg}")
