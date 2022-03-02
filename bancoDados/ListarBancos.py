ceaf0ba20cb1752127d4a051d470577dc497c542
Gabriel Sanches <thebug@outlook.com.br>
2022-03-02 19:13:09 -0300

    Executando a sentença sql que lista os bancos

---
+++ bancoDados/ListarBancos.py
@@ -0,0 +1,17 @@
+#!python3
+import mysql.connector
+
+from mysql.connector import connect
+
+conexao = connect(
+    host='localhost',
+    user='root',
+    passwd='qwerty',
+    port='3308'
+)
+# onde sera mandado o codigo a ser processado no banco
+cursor = conexao.cursor()
+cursor.execute('SHOW DATABASES')
+
+for i, database in enumerate(cursor, start=1):
+    print(f'Banco de dados {i}: {database[0]}')
